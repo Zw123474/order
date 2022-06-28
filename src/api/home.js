999658644f24bef3767ba8caac7c1e7b7b7983ba
gmy_git <1779717652@qq.com>
2022-06-28 19:33:28 +0800

    Home

---
+++ src/api/home.js
@@ -0,0 +1,7 @@
+import request from '@/utils/request'
+
+export const getMyChannels = () => {
+  return request({
+    url: 'user/channels'
+  })
+}
--- src/api/user.js
+++ src/api/user.js
@@ -1,5 +1,4 @@
 import request from '@/utils/request'
-import store from '@/store'
 export const getSmsCode = (mobile) => {
   return request({
     url: `/sms/codes/${mobile}`
@@ -17,9 +16,6 @@ export const login = ({ mobile, code }) => {
 }
 export const getUserInfo = () => {
   return request({
-    url: 'user',
-    headers: {
-      Authorization: 'Bearer ' + store.state.user.token
-    }
+    url: 'user'
   })
 }
